844daeece3e905804f3629714f121dfbb3202794
mhmdbhsk <muhammadbhaska0@gmail.com>
2023-12-07 03:38:01 +0700

    p

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ const router = require('./routes/index');
 const bodyParser = require('body-parser');
 
 // app.use(bodyParser.json());
-app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
 
 app.use(express.static('public'));
 app.use('/', router);
